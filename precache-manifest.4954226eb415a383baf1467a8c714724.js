0239c6bc727c21254a6f7d603018032141ff3502
Vinloy11 <a.shtork@mail.ru>
2020-01-13 16:58:50 +0700

    Updates

---
+++ precache-manifest.4954226eb415a383baf1467a8c714724.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a9f7cbc42891cd8e4039a50ec7c772",
+    "url": "/standard-todo/index.html"
+  },
+  {
+    "revision": "7e9e9c179d6644360db5",
+    "url": "/standard-todo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f4ea172ca1865dc0fa8",
+    "url": "/standard-todo/static/js/2.db1fcf39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/standard-todo/static/js/2.db1fcf39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e9e9c179d6644360db5",
+    "url": "/standard-todo/static/js/main.026423e7.chunk.js"
+  },
+  {
+    "revision": "864af1cdf2fdb88836c5",
+    "url": "/standard-todo/static/js/runtime-main.ac9d9115.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/standard-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
